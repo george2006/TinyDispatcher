4bb1fb91c28d9956c79c1eb12333f87219b93533
George <durbich@outlook.com>
2026-01-29 12:43:19 +0100

    added pipeline map descriptor

---
+++ src/TinyDispatcher.SourceGen/PipelineMap/PipelineDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TinyDispatcher.SourceGen.PipelineMap;
+
+internal sealed record PipelineDescriptor(
+    string CommandFullName,
+    string ContextFullName,
+    string HandlerFullName,
+    IReadOnlyList<MiddlewareDescriptor> Middlewares,
+    IReadOnlyList<string> PoliciesApplied);
+
+internal sealed record MiddlewareDescriptor(
+    string MiddlewareFullName,
+    string Source);
